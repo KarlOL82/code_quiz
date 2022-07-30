5e506a43936da7fe3b2cd51da970c1c72132ddd4
KarLOL82 <klinfeldt@gmail.com>
2022-07-29 21:04:09 -0700

    score is displaying but not as a list

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # code_quiz
 An in-browser quiz game with multiple choice questions and a scoreboard.
+
+## User Story
+As a bootcamp student,
+I want to create a quiz game to be played in the browser
+So that I can challenge my peers and compare their knowledge to my own.
+
+## Mission Statement
+The purpose of this project is to create dynamic content in the browser 
+
--- assets/js/score.js
+++ assets/js/score.js
@@ -2,19 +2,35 @@
  var userData;
  renderScore();
 
-function renderScore(event) {
-    // location.href = "./score.html";
-    var scoreEl = document.querySelector("#score");
-    var userData = JSON.parse(localStorage.getItem("nameScore"));
-    var userDisplay = JSON.stringify("nameScore");
-    window.location.href = "./score.html";
-    if (userData !== null) {
-      scoreEl.textContent = `Your Score is:   ${userDisplay}`;
-    } else {
-      return;
-    }
+// function renderScore(event) {
+//     // location.href = "./score.html";
+//     var scoreEl = document.querySelector("#score");
+//     var userData = JSON.parse(localStorage.getItem("nameScore"));
+//     var userDisplay = JSON.stringify("nameScore");
+//     window.location.href = "./score.html";
+//     if (userData !== null) {
+//       scoreEl.textContent = `Your Score is:   ${userDisplay}`;
+//         // scoreEl.textContent = userData.name;
+//         // scoreEl.textContent = userData.score;
+
+//     } else {
+//       return;
+//     }
   
     
-  }
+//   };
 
-  
+function renderScore() {
+    var scoreEl = document.querySelector(".score");
+    var userData = JSON.parse(localStorage.getItem("nameScore"));
+
+    for (var i = 0; i < userData.length; i++) {
+      var userDisplay = `Name: ${userData[i].name} | Score: ${userData[i].score}`;
+      if (userData[i] !== null) {
+        // scoreEl.textContent = userDisplay;
+        scoreEl.textContent += `${userDisplay}`;
+      } else {
+        return;
+      }
+    }
+}  
